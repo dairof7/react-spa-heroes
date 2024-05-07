31b32ebab37c82ce628d35edbd79d6ba3193e520
dairof7 <dairof7@gmail.com>
2024-05-07 10:08:25 -0500

    [add] adding files
    Signed-off-by: dairof7 <dairof7@gmail.com>

---
+++ src/heroes/pages/DcPage.jsx
@@ -0,0 +1,8 @@
+import { HeroList } from "../components"
+
+export const DcPage = () => {
+  return (
+    <HeroList publisher='DC Comics'/>
+  )
+}
+ 
---
+++ src/heroes/pages/MarvelPage.jsx
@@ -0,0 +1,7 @@
+import { HeroList } from "../components"
+
+export const MarvelPage = () => {
+  return (
+    <HeroList publisher='Marvel Comics'/>
+  )
+}
